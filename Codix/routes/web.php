4b3979b1c84fcc4d5c80e4d1bb6b64f67ae25156
NgoslabSamuel <ngoslabs55@gmail.com>
2026-03-14 12:53:28 +0000

    Updated resource route

--- Codix/routes/web.php
+++ Codix/routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GreetController;
+use App\Http\Controllers\TaskController;
 
 Route::get('/hello', function () {
     return 'Hello, Laravel!';
 });
 
 Route::get('/greet', [GreetController::class, 'show']);
+
+Route::resource('tasks', TaskController::class);
