9f1b3733441bba7245f55c6261f6301d90f8e270
NgoslabSamuel <ngoslabs55@gmail.com>
2026-03-14 13:01:10 +0000

    Added TaskController

---
+++ Codix/app/Http/Controllers/TaskController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Task::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+   public function store(Request $request)
+{
+    $task = Task::create([
+        'title' => $request->input('title'),
+        'description' => $request->input('description'),
+        'is_completed' => $request->input('is_completed', false),
+    ]);
+
+    return response()->json($task, 201);
+}
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return $task;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        $task->update($request->all());
+        return response()->json($task, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json(null, 204);
+    }
+}
